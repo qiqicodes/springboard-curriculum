c80459b1e34e7bbfea0249e105d5ebc2d8718205
qiqicodes <qiqizheng0127@gmail.com>
2021-04-19 15:02:06 -0700

    giphy js logics plans

--- unit-14-projects/ajax-api/giphy-party/app.js
+++ unit-14-projects/ajax-api/giphy-party/app.js
@@ -1,7 +1,19 @@
 console.log("Let's get this party started!");
-const removebtn = document.getElementById('remove-all-content');
-const gifdisplay = document.getElementById("gif-display")
+const removeBtn = $("#remove-all-content");
+const gifDisplay = $("#gif-display");
+const searchContent = $("#search-content");
 
-removebtn.addEventListener("click", function(){
-    gifdisplay.innerHTML="";
+// use ajax result data to add gif
+function addGif(data) {
+ 
+}
+
+// clear search box and make ajax call
+$("form").on("submit", function(event){
+    event.preventDefault();
 })
+
+// remove gif
+$(removeBtn).on("click", function(){
+    gifDisplay.empty();
+}) 
