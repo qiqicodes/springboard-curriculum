655fb357c3484a8f14b3ec8cc3e78026e5b297b5
qiqicodes <qiqizheng0127@gmail.com>
2021-04-19 17:49:40 -0700

    complete giphy party

--- unit-14-projects/ajax-api/giphy-party/app.js
+++ unit-14-projects/ajax-api/giphy-party/app.js
@@ -1,17 +1,42 @@
 console.log("Let's get this party started!");
+
 const removeBtn = $("#remove-all-content");
 const gifDisplay = $("#gif-display");
 const searchContent = $("#search-content");
 
 // use ajax result data to add gif
-function addGif(data) {
- 
+function addGif(result) {
+    let numResult = result.data.length;
+    if (numResult) {
+        let randomIndex = Math.floor(Math.random() * numResult);
+        let newCard = $("<div>", { class: "col-lg-4 col-12 mt-3 mb-3" });
+        let newGif = $("<img>", { 
+            class: "w-100", 
+            src: result.data[randomIndex].images.original.url 
+        });
+        newCard.append(newGif);
+        gifDisplay.append($(newCard));
+    }
 }
 
 // clear search box and make ajax call
-$("form").on("submit", function(event){
+$("form").on("submit", async function(event){
     event.preventDefault();
-})
+
+    let search = searchContent.val();
+    searchContent.val("");
+
+    // make call
+    const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
+        params: {
+            q: search,
+            api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+        }
+    });
+
+    // add gif
+    addGif(response.data);
+});
 
 // remove gif
 $(removeBtn).on("click", function(){
