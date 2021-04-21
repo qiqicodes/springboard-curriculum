426cfd9ef6a43f55b6a72231a15299c056899a9c
qiqicodes <qiqizheng0127@gmail.com>
2021-04-21 12:02:42 -0700

    init cra exercises

---
+++ unit-39-projects/es2015-module-exercise/src/helpers.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+// returns a randomly selected item from array of items
+const choice = (items) => {
+
+}
+
+// removes the first matching item from items, 
+// if item exists, and returns it. Otherwise returns undefined.
+const remove = (items, item) => {
+
+}
+
+
+export { choice, remove };
