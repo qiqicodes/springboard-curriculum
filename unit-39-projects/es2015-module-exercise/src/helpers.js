7c6d13799636786e849879e26eb099dd978eb4c3
qiqicodes <qiqizheng0127@gmail.com>
2021-04-21 12:17:30 -0700

    import, export modules

--- unit-39-projects/es2015-module-exercise/src/helpers.js
+++ unit-39-projects/es2015-module-exercise/src/helpers.js
@@ -1,15 +1,18 @@
-import React from "react"
-
 // returns a randomly selected item from array of items
 const choice = (items) => {
+    let index = Math.floor(Math.random()* items.length);
+    return items[index];
 
 }
 
 // removes the first matching item from items, 
 // if item exists, and returns it. Otherwise returns undefined.
 const remove = (items, item) => {
-
+    for(let i = 0; i < items.length; i++) {
+        if(items[i] === item){
+            return [...items.slice(0,i), ...items.slice(i+1)]; 
+        }
+    }
 }
 
-
-export { choice, remove };
+export { choice, remove }
---
+++ unit-39-projects/es2015-module-exercise/src/index.js
@@ -0,0 +1,20 @@
+import fruits from "./foods";
+import { choice, remove } from "./helpers";
+
+// This file should import the fruits and both array helpers. It should then:
+
+// Randomly draw a fruit from the array
+// Log the message “I’d like one RANDOMFRUIT, please.”
+// Log the message “Here you go: RANDOMFRUIT”
+// Log the message “Delicious! May I have another?”
+// Remove the fruit from the array of fruits
+// Log the message “I’m sorry, we’re all out. We have FRUITSLEFT left.”
+
+let randomFruit = choice(fruits);
+
+console.log(`I’d like one ${randomFruit}, please.`);
+console.log(`Here you go: ${randomFruit}`);
+console.log(`Delicious! May I have another?`);
+
+let fruitsLeft = remove(fruits, randomFruit);
+console.log(`I’m sorry, we’re all out. We have ${fruitsLeft} left.`);
