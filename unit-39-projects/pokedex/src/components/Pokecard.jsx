72920541d1c72e81f4be67b7fce4a9a779d8e6e3
qiqicodes <qiqizheng0127@gmail.com>
2021-04-21 15:16:31 -0700

    mvp pokedex version 1

--- unit-39-projects/pokedex/src/components/Pokecard.jsx
+++ unit-39-projects/pokedex/src/components/Pokecard.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import "../assets/Pokecard.css";
 
-const Pokecard = () => {
+const Poke_DB =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
-}
+// Shows a single Pokemon, with their name, image, and type.
+const Pokecard = (props) => {
+  let img = `${Poke_DB}${props.id}.png`;
+
+  return (
+    <div className="Pokecard">
+      <div className="Pokecard-name">{props.name}</div>
+      <img className="Pokecard-img" src={img}/>
+      <div className="Pokecard-type">TYPE: {props.type}</div>
+      <div className="Pokecard-exp">EXP: {props.exp}</div>
+    </div>
+  );
+};
 
 export default Pokecard;
