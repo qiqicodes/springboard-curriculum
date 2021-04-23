5b5035321a21fcc197eecdb3e71ff304ef69262a
qiqicodes <qiqizheng0127@gmail.com>
2021-04-23 12:16:53 -0700

    flask init app, get route and get requests

--- unit-19-projects/flask-greet-calc/calc/app.py
+++ unit-19-projects/flask-greet-calc/calc/app.py
@@ -1,1 +1,40 @@
-# Put your app in here.
+from Flask import flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+@app.route("/add")
+def addition():
+    """Add a and b."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    res = add(a, b)
+    return str(res)
+
+@app.route("/sub")
+def subtraction():
+    """Subtract b by a."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    res = sub(a, b)
+    return str(res)
+
+@app.route("/mult")
+def multiplication():
+    """Multiply a and b."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    res = mult(a, b)
+    return str(res)
+
+@app.route("/div")
+def division():
+    """Divide a by b."""
+    
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    res = div(a, b)
+    return str(res)
---
+++ unit-19-projects/flask-greet-calc/greet/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/welcome')
+def welcome():
+    return "welcome"
+
+@app.route('/welcome/home')
+def welcome_home():
+    return "welcome home"
+
+@app.route('/welcome/back')
+def welcome_back():
+    return "welcome back"
+
