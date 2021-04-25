21e5c6abc42fba4bf84e140ab15f741a3fdd049a
qiqicodes <qiqizheng0127@gmail.com>
2021-04-24 19:10:52 -0700

    finish survey with flash message and conditionals

--- unit-19-projects/flask-survey/app.py
+++ unit-19-projects/flask-survey/app.py
@@ -8,12 +8,60 @@ app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
 debug = DebugToolbarExtension(app)
 
+responses = []
+
+
 @app.route('/')
 def show_start_page():
     """Select a survey"""
     return render_template("start.html", survey=survey)
 
 @app.route('/start', methods=["POST"])
-def show_question_page():
-    """Check if redirect is happenly appropriately"""
-    return render_template("question.html")
+def start_survey():
+    """Directs user to first question page"""
+    return redirect("/questions/0")
+
+@app.route("/questions/<int:question_id>")
+def show_question(question_id):
+    """Show current question"""
+    # trying to access question page before the response list is initiated
+    if (responses is None):
+        return redirect("/")
+
+    # if all questions are answered
+    if (len(responses) == len(survey.questions)):
+        return redirect("/complete")
+
+    # step 7 flash message & ban out of order access
+    if (len(responses) != question_id):
+        flash(f"Invalid question id: {question_id}.")
+        return redirect(f"/questions/{len(responses)}")
+
+    question = survey.questions[question_id]
+    return render_template(
+        "question.html", question_num=question_id, question=question)
+
+
+@app.route('/answer', methods=["POST"])
+def handle_answer():
+    # get choice from form
+    choice = request.form["answer"]
+
+    # add to responses list
+    responses.append(choice)
+    
+
+    if (len(responses) == len(survey.questions)):
+    # They've answered all the questions! Thank them.
+        return redirect("/complete")
+
+    else:
+        return redirect(f"/questions/{len(responses)}")
+
+
+
+
+@app.route('/complete')
+def complete_survey():
+    """Direct user to completion page with a thank you note"""
+    return render_template("completion.html")
