27ca8289bf6194024b26f3f89890026b8c0b190f
qiqicodes <qiqizheng0127@gmail.com>
2021-06-17 13:36:18 -0700

    [main] functional api calls

--- unit-14-projects/ajax-api/tv-maze/tvmaze.js
+++ unit-14-projects/ajax-api/tv-maze/tvmaze.js
@@ -2,7 +2,6 @@
  *     { id, name, summary, episodesUrl }
  */
 
-
 /** Search Shows
  *    - given a search term, search for tv shows that
  *      match that query.  The function is async show it
@@ -17,21 +16,28 @@
         image: <an image from the show data, or a default imege if no image exists, (image isn't needed until later)>
       }
  */
+
+const GENERIC_IMAGE_URL = "https://tinyurl.com/tv-missing";
+
 async function searchShows(query) {
-  // TODO: Make an ajax request to the searchShows api.  Remove
-  // hard coded data.
+  // TODO: Make an ajax request to the searchShows api.  Remove hard coded data.
+  let response = await axios.get(
+    `http://api.tvmaze.com/search/shows?q=${query}`
+  );
 
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary: "<p><b>The Bletchley Circle</b> follows the journey of four ordinary women with extraordinary skills that helped to end World War II.</p><p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their normal lives, modestly setting aside the part they played in producing crucial intelligence, which helped the Allies to victory and shortened the war. When Susan discovers a hidden code behind an unsolved murder she is met by skepticism from the police. She quickly realises she can only begin to crack the murders and bring the culprit to justice with her former friends.</p>",
-      image: "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+  console.log(response.data);
+
+  let shows = response.data.map((result) => {
+    let show = result.show;
+    return {
+      id: show.id,
+      name: show.name,
+      summary: show.summary,
+      image: show.image ? show.image.medium : GENERIC_IMAGE_URL,
+    };
+  });
+  return shows;
 }
-
-
 
 /** Populate shows list:
  *     - given list of shows, add shows to DOM
@@ -45,25 +51,27 @@ function populateShows(shows) {
     let $item = $(
       `<div class="col-md-6 col-lg-3 Show" data-show-id="${show.id}">
          <div class="card" data-show-id="${show.id}">
+         <img class="card-img-top" src="${show.image}">
            <div class="card-body">
              <h5 class="card-title">${show.name}</h5>
              <p class="card-text">${show.summary}</p>
+             <button class="btn btn-success get-episodes">Episodes</button>
            </div>
          </div>
        </div>
-      `);
+      `
+    );
 
     $showsList.append($item);
   }
 }
-
 
 /** Handle search form submission:
  *    - hide episodes area
  *    - get list of matching shows and show in shows list
  */
 
-$("#search-form").on("submit", async function handleSearch (evt) {
+$("#search-form").on("submit", async function handleSearch(evt) {
   evt.preventDefault();
 
   let query = $("#search-query").val();
@@ -76,7 +84,6 @@ $("#search-form").on("submit", async function handleSearch (evt) {
   populateShows(shows);
 });
 
-
 /** Given a show ID, return list of episodes:
  *      { id, name, season, number }
  */
@@ -85,6 +92,52 @@ async function getEpisodes(id) {
   // TODO: get episodes from tvmaze
   //       you can get this by making GET request to
   //       http://api.tvmaze.com/shows/SHOW-ID-HERE/episodes
-
+  let response = await axios.get(`http://api.tvmaze.com/shows/${id}/episodes`);
+  console.log(response.data);
   // TODO: return array-of-episode-info, as described in docstring above
+
+  let episodes = response.data.map((result) => {
+    return {
+      id: result.id,
+      name: result.name,
+      season: result.season,
+      number: result.number,
+      airdate: result.airdate,
+    };
+  });
+  return episodes;
 }
+
+function populateEpisodes(episodes) {
+  const $episodesList = $("#episodes-list");
+  $episodesList.empty();
+
+  for (let episode of episodes) {
+    let $item = $(
+      `<li>
+        ${episode.name}
+        (season ${episode.season}, episode ${episode.number})
+        airdate ${episode.airdate}
+      </li>
+      `
+    );
+
+    $episodesList.append($item);
+  }
+  $("#episodes-area").show();
+}
+
+/** Handle search form submission:
+ *    - hide episodes area
+ *    - get list of matching shows and show in shows list
+ */
+
+$("#shows-list").on(
+  "click",
+  ".get-episodes",
+  async function handleEpisodes(evt) {
+    let showID = $(evt.target).closest(".Show").data("show-id");
+    let episodes = await getEpisodes(showID);
+    populateEpisodes(episodes);
+  }
+);
