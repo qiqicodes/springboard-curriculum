6c0e3142968817771dfa296e007e69ebf14b3359
qiqicodes <qiqizheng0127@gmail.com>
2021-08-29 19:41:59 -0700

    [main] success count zeroes

--- unit-47-data-structure/3-divide-and-conquer/count-zeroes.js
+++ unit-47-data-structure/3-divide-and-conquer/count-zeroes.js
@@ -5,12 +5,40 @@
 // Time Complexity: O(log N)
 
 function countZeroes(arr) {
-  let Left, Right;
+  // [1,1,1,1,0,0]
+  let left = 0;
+  let right = arr.length - 1; //5
+  let mid;
+  let numZeroes = 0;
+
+  if (arr[left] === 0) {
+    numZeroes = arr.length;
+  }
+
+  while (left <= right) {
+    //3<=5
+    mid = Math.floor((left + right) / 2); //4
+    if (arr[mid] === 0) {
+      //0
+      right = mid - 1; //3
+      if (arr[right] === 1) {
+        //t
+        numZeroes = arr.length - mid;
+      }
+    } else {
+      //1
+      left = mid + 1; //3
+      if (arr[left] === 0) {
+        numZeroes = arr.length - left;
+      }
+    }
+  }
+  return numZeroes;
 }
 
 module.exports = countZeroes;
 
-countZeroes([1, 1, 1, 1, 0, 0]); // 2
-countZeroes([1, 0, 0, 0, 0]); // 4
-countZeroes([0, 0, 0]); // 3
-countZeroes([1, 1, 1, 1]); // 0
+// countZeroes([1, 1, 1, 1, 0, 0]); // 2
+// countZeroes([1, 0, 0, 0, 0]); // 4
+// countZeroes([0, 0, 0]); // 3
+// countZeroes([1, 1, 1, 1]); //0
