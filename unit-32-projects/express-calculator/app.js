d1767cda8ef4684499d70b8a669acf700bfc4759
qiqicodes <qiqizheng0127@gmail.com>
2021-09-17 22:58:51 -0700

    [main] instruction for calculator

--- unit-32-projects/express-calculator/app.js
+++ unit-32-projects/express-calculator/app.js
@@ -9,9 +9,26 @@ const {
   findMode,
 } = require("./helpers");
 
-// TODO: GET routes
+app.get("/", (req, res) => {
+  const result = `Hello, Dear! 
+  Let's find your mean, median, and mode!
+  Please enter your numbers in the form of a comma-separated list in the URL.
+  For example: 1,2,3,4,5,6,7,8,9,10
+  =]
 
+  Fill in your query like this: /mean?nums=1,2,3,4`;
+
+  res.send(result);
+});
+// TODO: GET routes
 app.get("/mean", (req, res, next) => {
+  // Todo: handle error
+  // if (!req.query.nums)
+  //   return new ExpressError(
+  //     "Please pass in your query to nums with a list of numbers separated by comma"
+  //   );
+
+  console.log(req);
   console.log(req.query.nums);
   nums = req.query.nums.split(",");
   console.log(nums);
@@ -20,11 +37,15 @@ app.get("/mean", (req, res, next) => {
   return res.send(result);
 });
 
-// TODO:
-app.get("/median", (req, res, next) => {});
+// TODO: GET routes
+app.get("/median", (req, res, next) => {
+  // Todo: handle error
+});
 
-// TODO:
-app.get("/mode", (req, res, next) => {});
+// TODO: GET routes
+app.get("/mode", (req, res, next) => {
+  // Todo: handle error
+});
 
 app.use(function (req, res, next) {
   const notFoundError = new ExpressError("Not Found", 404);
