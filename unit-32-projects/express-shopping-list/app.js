b88b8e64e1beeb7b4261cd1c2d22ada94094b1dd
qiqicodes <qiqizheng0127@gmail.com>
2021-09-18 23:09:58 -0700

    [main] set error handlers

--- unit-32-projects/express-shopping-list/app.js
+++ unit-32-projects/express-shopping-list/app.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const ExpressError = require("./expressError");
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.use(function (req, res, next) {
+  return new ExpressError("Not Found", 404);
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+
+  return res.json({
+    status: err.status,
+    message: err.message,
+  });
 });
 
 app.listen(3000, () => {
